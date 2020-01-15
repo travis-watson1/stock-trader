1dcbe0ecf982d2867a38dcf1d1e140dcc5ddd4ef
Travis Watson <watsonjamest@gmail.com>
2020-01-15 16:26:07 -0600

    Fix error with express server and heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
-const app = express();
-
-app.use('/', serveStatic(path.join(__dirname, 'dist')))
-
-const port = process.env.PORT || 8080
-app.listen(port)
-
-console.log('Listening on port: ' + port)
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+ 
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html")
+})
+ 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
